5dc587303bb8160b0f0365feed721378c05ffc40
shtakai <shtakai@gmail.com>
2016-08-11 19:29:54 +0900

    add flask-login

--- waitercaller.py
+++ waitercaller.py
@@ -1,11 +1,20 @@
 from flask import Flask
+from flask.ext.login import LoginManager
+from flask.ext.login import login_required
 
 app = Flask(__name__)
+login_manager = LoginManager(app)
 
 
 @app.route('/')
 def home():
     return "Under construction"
+
+
+@app.route('/account')
+@login_required
+def account():
+    return "You are logged in"
 
 
 if __name__ == '__main__':
